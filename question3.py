40d87eff32f1d2b034e36ecfab2977700924813a
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-03-12 17:22:42 +0330

    implemented closest_Pair

--- question3.py
+++ question3.py
@@ -27,5 +27,42 @@ class node:
         return p, q, dist
 
 
+    def closest_Pair(self:list, lst_x:list, lst_y:list):
+        n= len(lst_x)
+        if n==1:
+            print("At least n should be 2")
+        if n==2:
+            return lst_x[0].dist(lst_x[1])
+
+        if n==3:
+            return self.closest_pair_classic(lst_x, lst_y)
+
+
+        mid = lst_x[n//2]
+        lst_x_left = lst_x[:n//2]
+        lst_x_right = lst_x[n//2:]
+        lst_y_left = lst_y.filter(lambda point : point.x < mid.x)
+        lst_y_right = lst_y - lst_y_left
+
+        p_left, q_left, delta_left = self.closest_Pair(lst_x_left, lst_y_left)
+        p_right, q_right, delta_right = self.closest_Pair(lst_x_right, lst_y_right)
+        p, q, delta = p_left, q_left, delta_left if delta_left < delta_right else delta_right
+
+        doubted_points = [point for point in lst_y if mid.x - delta < point.x < mid.x + delta]
+
+        for i in range(len(doubted_points)):
+            for j in range(i+1,min(i+7, len(doubted_points))):
+                delta_p = doubted_points[i].dist(doubted_points[j])
+                if delta_p < delta:
+                    p, q, delta = doubted_points[i], doubted_points[j], delta_p
+
+
+        return p, q, delta
+
+
+
+
+
+
 
 
